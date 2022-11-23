dd0c8d21b143ac0ec8d1ff9a49e2e95c365a785a
FanncuS4n <118911524+FanncuS4n@users.noreply.github.com>
2022-11-23 11:18:38 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <conio.h>
+#include <string.h>
+#include "rlutil.h"
+#include "funcionesPuntos.h"
+#include "repertorioVisual.h"
+#include "unJugador.h"
+#include "dosJugadores.h"
+#include "dibujar_dados.h"
+
+using namespace std;
+
+
+
+int main(){
+
+   SetConsoleTitle("Diez Mil");
+
+   menu();
+
+    return 0;
+}
